c3bd49803bce56359de86494ca935b37e111d898
dersar00 <dersar00@gmail.com>
2018-06-17 23:24:33 +0300

    fix bug

--- app/controllers/api/users/sessions_controller.rb
+++ app/controllers/api/users/sessions_controller.rb
@@ -5,7 +5,7 @@ class Api::Users::SessionsController < Devise::SessionsController
 def create
   user = warden.authenticate!(auth_options)
   token = Tiddle.create_and_return_token(user, request)
-  render json: { authentication_token: token email: user.email }
+  render json: { authentication_token: token, email: user.email }
 end
 
 def destroy
